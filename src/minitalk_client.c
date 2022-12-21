2087d95e853dceb9aed9c4627b3150744c4fe4df
Basile Jeannot <basiljeannot@gmail.com>
2022-12-21 17:07:25 +0100

    ça marche mais c'est delayed

--- src/minitalk_client.c
+++ src/minitalk_client.c
@@ -6,7 +6,7 @@
 /*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 10:13:50 by bajeanno          #+#    #+#             */
-/*   Updated: 2022/12/21 16:15:10 by bajeanno         ###   ########lyon.fr   */
+/*   Updated: 2022/12/21 17:04:46 by bajeanno         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int	main(int argc, char **argv)
 	i = 0;
 	while (argv[2][i])
 	{
-		j = 1;
+		j = 0;
 		while (j < 8)
 		{
-			if ((argv[1][i] & (1 << (7 - j))) == 0)
+			if (((argv[2][i] << j) & 0b10000000) == 0)
 			{
 				if (kill(pid, SIGUSR1) != 0)
 					return (write(STDERR_FILENO, "the specified process is not running anymore or the PID is incorrect\n", 69), 1);
@@ -46,9 +46,7 @@ int	main(int argc, char **argv)
 		// ft_printf("\n");
 	}
 	while (1)
-	{
 		if (kill(pid, SIGUSR1) != 0)
 			return (0);
-	}
 	return (0);
 }
