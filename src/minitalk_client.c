516ef707909fa2a42b0115daab293b8e44ba81f0
Basile Jeannot <basiljeannot@gmail.com>
2023-01-04 22:02:30 +0100

    original = delay version ; bonus = callback version

---
+++ src/minitalk_client.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_client.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 10:13:50 by bajeanno          #+#    #+#             */
+/*   Updated: 2022/12/21 17:56:06 by bajeanno         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+static int	send_byte(char byte, int pid)
+{
+	size_t	j;
+
+	j = 0;
+	while (j < 8)
+	{
+		if (((byte << j) & 0b10000000) == 0)
+		{
+			if (kill(pid, SIGUSR1) != 0)
+				return (write(STDERR_FILENO,"PID is incorrect\n",17), 1);
+		}
+		else if (kill(pid, SIGUSR2) != 0)
+			return (write(STDERR_FILENO,"PID is incorrect\n", 17), 1);
+		j++;
+		usleep(20);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	size_t	i;
+	int		pid;
+
+	if (argc != 3)
+		return (write(1, "Error\n", 6), 1);
+	pid = ft_atoi(argv[1]);
+	i = 0;
+	while (argv[2][i])
+	{
+		if (send_byte(argv[2][i], pid))
+			return (1);
+		i++;
+	}
+	if (send_byte(0, pid))
+		return (1);
+	return (0);
+}
---
+++ src/minitalk_server.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_server.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 09:58:37 by bajeanno          #+#    #+#             */
+/*   Updated: 2023/01/04 02:59:40 by bajeanno         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+#include <unistd.h>
+
+t_string	g_string;
+
+static void	ft_init_string()
+{
+	g_string.str = ft_calloc(g_string.size, sizeof(char));
+	g_string.size = 1;
+	g_string.count = 0;
+	g_string.finished = 0;
+}
+
+void	update_string(int sig)
+{
+	char	*newstr;
+
+	g_string.str[g_string.size - 1] <<= 1;
+	g_string.str[g_string.size - 1] |= (sig == SIGUSR2);
+	g_string.count++;
+	if (g_string.count == 8 && g_string.str[g_string.size - 1] == 0)
+		return (g_string.finished = 1, (void)0);
+	if (g_string.count == 8)
+	{
+		g_string.size++;
+		newstr = ft_calloc(g_string.size, sizeof (char));
+		if (!newstr)
+			return ;
+		ft_memmove(newstr, g_string.str, g_string.size - 1);
+		if (g_string.str)
+			free(g_string.str);
+		g_string.str = newstr;
+		g_string.count = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_init_string();
+	signal(SIGUSR1, update_string);
+	signal(SIGUSR2, update_string);
+	ft_printf("%d\n", getpid());
+	while (1)
+	{
+		if (g_string.finished)
+		{
+			ft_printf("%s\n", g_string.str);
+			g_string.finished = 0;
+			free(g_string.str);
+			ft_init_string();
+		}
+	}
+	return (0);
+}
